11c873de7e445c95a9e9da9ce1609bed5d3d8efd
kess-map <kdierunaba@mapis4u.com>
2025-10-07 01:09:57 +0100

    update

--- backend/middleware/isAdmin.js
+++ backend/middleware/isAdmin.js
@@ -1,9 +1,5 @@
-import User from "../models/userModel.js";
-
 export const isAdmin = async(req, res, next)=>{
-    const user = await User.findById(req.userId)
-
-    if(user.role !== 'ADMIN'){
+    if(!req.isAdmin){
         return res.status(403).json({message: 'Unauthorized - You cannot access this route'})
     }
 
